69b33614801da7a6d4cf84342feed889ecbe6a5a
Emre Geçit <emregecit5@gmail.com>
2024-12-12 21:14:30 +0300

    Refactor binary message handling in MyCovertChannel to process 3-bit slices

--- code/MyCovertChannel.py
+++ code/MyCovertChannel.py
@@ -29,39 +29,44 @@ class MyCovertChannel(CovertChannelBase):
         binary_message = self.generate_random_binary_message_with_logging(log_file_name)
 
         # Ensure the binary message is 3 bits long
-        binary_message = binary_message[:3].zfill(3)
+        binary_message = binary_message  # [:3].zfill(3)
 
-        # Create a raw socket
-        sock = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_RAW)
+        for binary_message_sliced in [
+            binary_message[i : i + 3] for i in range(0, len(binary_message), 3)
+        ]:
+            # Create a raw socket
+            sock = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_RAW)
 
-        # IP header fields
-        ip_header = struct.pack(
-            "!BBHHHBBH4s4s",
-            69,  # Version and IHL
-            0,  # Type of Service
-            20,  # Total Length
-            54321,  # Identification
-            0,  # Flags and Fragment Offset
-            255,  # TTL
-            socket.IPPROTO_TCP,  # Protocol
-            0,  # Header Checksum
-            socket.inet_aton(source),  # Source Address
-            socket.inet_aton(destination),  # Destination Address
-        )
+            # IP header fields
+            ip_header = struct.pack(
+                "!BBHHHBBH4s4s",
+                69,  # Version and IHL
+                0,  # Type of Service
+                20,  # Total Length
+                54321,  # Identification
+                0,  # Flags and Fragment Offset
+                255,  # TTL
+                socket.IPPROTO_TCP,  # Protocol
+                0,  # Header Checksum
+                socket.inet_aton(source),  # Source Address
+                socket.inet_aton(destination),  # Destination Address
+            )
 
-        # Embed the binary message into the reserved flag field
-        reserved_flag = int(binary_message, 2) << 13
-        flags_and_fragment_offset = struct.unpack("!H", ip_header[6:8])[0]
-        flags_and_fragment_offset |= reserved_flag
-        ip_header = (
-            ip_header[:6] + struct.pack("!H", flags_and_fragment_offset) + ip_header[8:]
-        )
+            # Embed the binary message into the reserved flag field
+            reserved_flag = int(binary_message_sliced, 2) << 13
+            flags_and_fragment_offset = struct.unpack("!H", ip_header[6:8])[0]
+            flags_and_fragment_offset |= reserved_flag
+            ip_header = (
+                ip_header[:6]
+                + struct.pack("!H", flags_and_fragment_offset)
+                + ip_header[8:]
+            )
 
-        # Send the packet
-        sock.sendto(ip_header, (destination, 0))
+            # Send the packet
+            sock.sendto(ip_header, (destination, 0))
 
-        # Log the sent message
-        self.log_message(binary_message, log_file_name)
+            # Log the sent message
+            self.log_message(binary_message_sliced, log_file_name)
         print("Message sent.")
 
     def process_packet(self, packet):
